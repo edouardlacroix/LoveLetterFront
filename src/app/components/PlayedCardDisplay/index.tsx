ef51d1c1d4666a3fd007d31807d66f055a964862
Edouard LACROIX <edouard.lacroix1@gmail.com>
2019-12-24 11:42:12 +0100

    PlayedCard list - display and popHover

--- src/app/components/PlayedCardDisplay/index.tsx
+++ src/app/components/PlayedCardDisplay/index.tsx
@@ -8,11 +8,26 @@ class PlayedCardDisplay extends Component {
 
   }
 
+  cardList = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }]
+
+
   render() {
     return (
       <div className={'playedCardDisplay-wrapper'} >
-        <Card />
-      </div>
+        <h2>Cards Played</h2>
+        <div className={'card-list'}>
+
+          {this.cardList.map((item, id) => {
+            return (
+              <div style={{ zIndex: 10 + id + 1, top: 10 + id * 70 + 'px' }} className={'card-alone'}>
+                <Card />
+              </div>
+            )
+          })
+          }
+        </div>
+
+      </div >
     );
   }
 }

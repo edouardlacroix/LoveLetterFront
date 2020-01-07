dba75e847f5a267ab9dc4fb56cd8264c6da71cd1
Edouard Lacroix <edouard.lacroix@cognizant.com>
2020-01-07 14:25:29 +0100

    Rerender playerlist on gamedata change

--- src/app/components/PlayerList/index.tsx
+++ src/app/components/PlayerList/index.tsx
@@ -12,18 +12,25 @@ class PlayerList extends Component {
     // SocketConnection.emit('UPDATE_GAME_DATA_REFRESH');
   }
 
-  localPlayer = this.props.gameData.filter(item => item.id === SocketConnection.id)[0]
-  otherPlayers = this.props.gameData.filter(item => item.id != SocketConnection.id)
   render() {
-    console.log(this.localPlayer)
+    const localPlayer = this.props.gameData.filter(
+      item => item.id === SocketConnection.id
+    )[0];
+    const otherPlayers = this.props.gameData.filter(
+      item => item.id != SocketConnection.id
+    );
+    console.log(localPlayer);
     return (
       <div className={'playerList-wrapper'}>
         <h2>Player List</h2>
         <div className={'localPlayer'}>
-          <h3><a>★</a>{this.localPlayer.name}</h3>
-          <p>Points : {this.localPlayer.points}</p>
+          <h3>
+            <a>★</a>
+            {localPlayer.name}
+          </h3>
+          <p>Points : {localPlayer.points}</p>
         </div>
-        {this.otherPlayers.map(item => (
+        {otherPlayers.map(item => (
           <div>
             <h3>{item.name}</h3>
             <p>Points : {item.points}</p>
